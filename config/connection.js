3ed419c9082ab85718814de1781e089a15ead5ae
JerrickJohnson <jerrickjohnson88@gmail.com>
2023-07-07 15:38:12 -0500

    updated connection

--- config/connection.js
+++ config/connection.js
@@ -2,11 +2,11 @@
 const mongoose = require('mongoose');
 // Connecting to the MongoDB database using the MongoDB URI provided in the environment 
 // variables or using the default URI if the environment variable is not set
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/SocialNetDB',{
-    useNewUrlParser: true, 
-    useUnifiedTopology: true,
-});
-
+// mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/SocialNetDB',{
+//     useNewUrlParser: true, 
+//     useUnifiedTopology: true,
+// });
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/your-database-name');
 // Exporting the connection to the database as a module
 module.exports = mongoose.connection
 
